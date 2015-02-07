1fde421b6777e1d3f2ec9e7472f91500ce387d40
miraks <a@vldkn.net>
2015-02-07 17:31:56 +0300

    allow notifications without message field and some formatting fixes

--- src/android/com/plugin/gcm/PushHandlerActivity.java
+++ src/android/com/plugin/gcm/PushHandlerActivity.java
@@ -10,14 +10,14 @@ import android.util.Log;
 
 public class PushHandlerActivity extends Activity
 {
-	private static String TAG = "PushHandlerActivity"; 
+	private static String TAG = "PushHandlerActivity";
 
 	/*
-	 * this activity will be started if the user touches a notification that we own. 
-	 * We send it's data off to the push plugin for processing.
-	 * If needed, we boot up the main activity to kickstart the application. 
-	 * @see android.app.Activity#onCreate(android.os.Bundle)
-	 */
+	* this activity will be started if the user touches a notification that we own.
+	* We send it's data off to the push plugin for processing.
+	* If needed, we boot up the main activity to kickstart the application.
+	* @see android.app.Activity#onCreate(android.os.Bundle)
+	*/
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -35,38 +35,38 @@ public class PushHandlerActivity extends Activity
 	}
 
 	/**
-	 * Takes the pushBundle extras from the intent, 
-	 * and sends it through to the PushPlugin for processing.
-	 */
+	* Takes the pushBundle extras from the intent,
+	* and sends it through to the PushPlugin for processing.
+	*/
 	private void processPushBundle(boolean isPushPluginActive)
 	{
 		Bundle extras = getIntent().getExtras();
 
 		if (extras != null)	{
 			Bundle originalExtras = extras.getBundle("pushBundle");
-            
-            originalExtras.putBoolean("foreground", false);
-            originalExtras.putBoolean("coldstart", !isPushPluginActive);
+
+			originalExtras.putBoolean("foreground", false);
+			originalExtras.putBoolean("coldstart", !isPushPluginActive);
 
 			PushPlugin.sendExtras(originalExtras);
 		}
 	}
 
 	/**
-	 * Forces the main activity to re-launch if it's unloaded.
-	 */
+	* Forces the main activity to re-launch if it's unloaded.
+	*/
 	private void forceMainActivityReload()
 	{
 		PackageManager pm = getPackageManager();
-		Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());    		
+		Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());
 		startActivity(launchIntent);
 	}
 
-  @Override
-  protected void onResume() {
-    super.onResume();
-    final NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-    notificationManager.cancelAll();
-  }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		final NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.cancelAll();
+	}
 
 }
